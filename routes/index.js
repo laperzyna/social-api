f16228363c4ae6194fe6fe9ff4bb702ce8b56c1e
laperzyna <90733086+laperzyna@users.noreply.github.com>
2022-08-16 09:31:48 -0700

    added routes following the route set up on the assignment page. Need to figure out the connections and test.

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+// route to use api routes
+router.use('/api', apiRoutes);
+
+// error handling
+router.use((req, res) => {
+    res.status(404).send('<h1>404 Error....</h1>');
+  });
+
+module.exports = router;
